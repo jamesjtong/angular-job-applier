d69ab386612be6d7fbbf14a3eaae8c5c5938c4de
jamesjtong <jtong33@gmail.com>
2014-01-16 19:47:44 -0500

    starts app with angular generator yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ultimateJobApplierApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
