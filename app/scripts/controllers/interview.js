694ed956bcc876741252005380afb72f39ca1c74
jamesjtong <jtong33@gmail.com>
2014-01-23 13:18:57 -0500

    adds timer

--- app/scripts/controllers/interview.js
+++ app/scripts/controllers/interview.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ultimateJobApplierApp')
-  .controller('InterviewCtrl', function ($scope) {
+  .controller('InterviewCtrl', function ($scope, $timeout) {
     $scope.interviewQuestions = [
       'Tell me about yourself.',
       'Why do you want this position?',
@@ -14,4 +14,16 @@ angular.module('ultimateJobApplierApp')
     $scope.getQuestion = function(){
       $scope.question.value = $scope.interviewQuestions.shift();
     };
+    
+    $scope.counter = 0;
+    $scope.onTimeout = function(){
+      $scope.counter++;
+      mytimeout = $timeout($scope.onTimeout,1000);
+    };
+    var mytimeout = $timeout($scope.onTimeout,1000);
+    
+    $scope.stop = function(){
+      $timeout.cancel(mytimeout);
+    };
+            
   });
