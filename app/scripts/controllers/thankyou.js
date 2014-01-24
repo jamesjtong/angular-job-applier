efc11645637daaa66aaa2da2400ea8998ff6b048
jamesjtong <jtong33@gmail.com>
2014-01-24 13:38:26 -0500

    adds thank you template based on cover letter template

--- app/scripts/controllers/thankyou.js
+++ app/scripts/controllers/thankyou.js
@@ -2,9 +2,18 @@
 
 angular.module('ultimateJobApplierApp')
   .controller('ThankyouCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.firstP = [{name: ''},{name: ''},{name: ''}];
+    $scope.secondP = [{name: ''},{name: ''},{name: ''}];
+    $scope.addP1 = function(){
+      $scope.firstP.push({name: ''});
+    };
+    $scope.addP2 = function(){
+      $scope.secondP.push({name: ''});
+    };
+    $scope.removeP1 = function(){
+      $scope.firstP.pop();
+    };
+    $scope.removeP2 = function(){
+      $scope.secondP.pop();
+    };
   });
