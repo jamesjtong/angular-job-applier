9acff300548f5e502f743df91221138b5fb14c9e
jamesjtong <jtong33@gmail.com>
2014-02-05 18:07:41 -0500

    adds underscore through bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -317,7 +317,9 @@ module.exports = function (grunt) {
 
     // Test settings
     karma: {
-
+      // e2e: {
+      //   configFile: 'karma-e2e.conf.js'
+      // },
       unit: {
         configFile: 'karma.conf.js',
         singleRun: false
