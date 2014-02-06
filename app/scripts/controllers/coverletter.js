8ac54513a59a2f914777ea7626cef66ce25168cd
jamesjtong <jtong33@gmail.com>
2014-02-06 00:33:48 -0500

    reformats scripts to 2 space indent and rebuilds grunt to deploy

--- app/scripts/controllers/coverletter.js
+++ app/scripts/controllers/coverletter.js
@@ -17,12 +17,11 @@ angular.module('ultimateJobApplierApp')
       $scope.secondP.pop();
     };
     $scope.generateLetter = function() {
-        var articleText = angular.element('article').text();
-        return articleText;
+      var articleText = angular.element('article').text();
+      return articleText;
     };
     
     $scope.doSomething = function () {
-        console.log('NgClip...');
+      console.log('NgClip...');
     };
-
   });
