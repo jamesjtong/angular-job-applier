2413fa8a52555c7660285d665e0f21f84b75f4dd
jamesjtong <jtong33@gmail.com>
2014-02-06 16:37:42 -0500

    refactors multicapitalize info capitalize filter by adding a parameter (from now on, just specify capitalize:"multi" if you want to capitalize all words in an input

--- app/scripts/filters/capitalize.js
+++ app/scripts/filters/capitalize.js
@@ -2,7 +2,16 @@
 
 angular.module('ultimateJobApplierApp')
   .filter('capitalize', function () {
-    return function (input) {
-      return input.substring(0,1).toUpperCase()+input.substring(1);
+    return function (input, type) {
+      if (type==='multi'){
+        var eachWord= input.split(' ');
+        var modifiedArr=[];
+        for (var i=0;i<eachWord.length;i++){
+          modifiedArr.push(eachWord[i].substring(0,1 ).toUpperCase()+eachWord[i].substring(1));
+        }
+        return modifiedArr.join(' ');
+      } else {
+        return input.substring(0,1).toUpperCase()+input.substring(1);
+      }
     };
   });
